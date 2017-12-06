e46441ab6a1ec31f3fc60bd247bfa092777378a3
Alexander Zavyalov <whmordred@gmail.com>
2017-12-06 11:00:19 +0800

    Uploading files

--- src/FileUploader.php
+++ src/FileUploader.php
@@ -4,5 +4,67 @@ namespace Voerro\FileUploader;
 
 class FileUploader
 {
-    //
+    /**
+     * $file an instance of UploadedFile
+     *
+     * @var Illuminate\Http\UploadedFile
+     */
+    protected $file;
+
+    /**
+     * $image an instance of Intervention Image
+     *
+     * @var Intervention\Image\Facades\Image
+     */
+    protected $image;
+
+    /**
+     * Custom constructor
+     *
+     * @param Illuminate\Http\UploadedFile $file
+     * @return Voerro\FileUploader\FileUploader
+     */
+    public static function make($file)
+    {
+        return (new FileUploader())->setFile($file);
+    }
+
+    /**
+     * Sets the $file property
+     *
+     * @param Illuminate\Http\UploadedFile $file
+     * @return Voerro\FileUploader\FileUploader $this
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * Uploads a file under a hashname
+     *
+     * @param string $path path to upload to
+     * @return string uploaded file's path
+     */
+    public function upload($path = '', $storage = 'public')
+    {
+        return $this->uploadAs('', $path, $storage);
+    }
+
+    /**
+     * Uploads a file under a speciifed name
+     *
+     * @param string $path path to upload to
+     * @return string uploaded file's path
+     */
+    public function uploadAs($filename, $path = '', $storage = 'public')
+    {
+        $filename = $filename
+            ? $filename . '.' . $this->file->getClientOriginalExtension()
+            : $this->file->hashName();
+
+        return $this->file->storeAs($path, $filename, $storage);
+    }
 }
--- tests/FileUploaderTest.php
+++ tests/FileUploaderTest.php
@@ -2,12 +2,42 @@
 
 namespace Voerro\FileUploader\Test;
 
-use FileUploader;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+use Voerro\FileUploader\FileUploader;
 
 class FileUploaderTest extends TestCase
 {
-    public function testExample()
+    // TODO;
+    // downsizing an image before uploading
+    // replace file
+    // replace file as
+
+    public function testItUploadsFiles()
     {
-        $this->assertTrue(true);
+        Storage::fake('public');
+
+        $file = UploadedFile::fake()->create('file.pdf', 100);
+
+        FileUploader::make($file)->upload();
+
+        Storage::disk('public')->assertExists($file->hashName());
+
+        FileUploader::make($file)->upload('documents/');
+
+        Storage::disk('public')->assertExists('documents/' . $file->hashName());
+    }
+
+    public function testItUploadsFilesUnderSpecifiedNames()
+    {
+        Storage::fake('public');
+
+        $file = UploadedFile::fake()->create('file.pdf', 100);
+
+        $filename = 'document';
+
+        FileUploader::make($file)->uploadAs($filename);
+
+        Storage::disk('public')->assertExists("{$filename}.pdf");
     }
 }
